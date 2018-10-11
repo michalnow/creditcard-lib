c134a1dea680ab7ef136673eb9b79c7fb3957b5b
Michał Nowakowski <michal951now@gmail.com>
2018-10-11 10:34:02 +0000

    added two methotds assignLimit and getLimit

---
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -0,0 +1,12 @@
+package pl.jkan.creditcard;
+
+public class CreditCard{
+    
+    public void assignLimit(double limit){
+        
+    }
+    
+    public double getLimit(){
+        return 2000;
+    }
+}
