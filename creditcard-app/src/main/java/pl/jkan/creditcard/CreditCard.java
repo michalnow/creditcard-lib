17d0c99ae659aafcb45e642c90bc714ffa7d6a7a
Michał Nowakowski <michal951now@gmail.com>
2018-10-18 10:13:19 +0000

    added commment od getLimit method

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -1,11 +1,14 @@
 package pl.jkan.creditcard;
 
 public class CreditCard{
-    
+
     public void assignLimit(double limit){
-        
+
     }
-    
+
+
+    //this method will return credit limit
+    //assigned to card
     public double getLimit(){
         return 2000;
     }
