bdaaf5d50893bdbe4f538988636dc06100ea8d37
Michał <michal951now@gmail.com>
2018-10-25 10:28:24 +0000

    implementation of withdraw

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -2,11 +2,24 @@ package pl.jkan.creditcard;
 
 public class CreditCard{
     
+    private double balance;
+    private double cardLimit;
+    
     public void assignLimit(double limit){
-        
+        balance = limit;
+        cardLimit = limit;
     }
     
     public double getLimit(){
         return 2000;
+    }
+    
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+    
+    public double getBalance() {
+        return balance;
     }
 }
