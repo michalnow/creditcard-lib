840dab14b85eb95dce9d16ae7b43227ca3050d01
Michał <michal951now@gmail.com>
2018-10-25 10:02:06 +0000

    card blocking implementation

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -2,11 +2,21 @@ package pl.jkan.creditcard;
 
 public class CreditCard{
     
+    private boolean isBlocked;
+    
     public void assignLimit(double limit){
         
     }
     
     public double getLimit(){
         return 2000;
+    }
+    
+    public void block(){
+        isBlocked = true;  
+    }
+    
+    public boolean isBlocked(){
+        return isBlocked;
     }
 }
