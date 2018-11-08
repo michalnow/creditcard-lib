6dddb97e48b063dbc023190ef34fa0801003223b
Michał <michal951now@gmail.com>
2018-11-08 13:30:50 +0000

    added 2 constructors

--- creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -1,38 +1,52 @@
 package pl.jkan.creditcard;
 
 public class CreditCard{
-    
+
 
     private boolean isBlocked;
 
     private double balance;
     private double cardLimit;
+    private String cardNumber;
 
-    
+    public CreditCard(){
+        this.cardNumber = "random_nmber";
+    }
+
+    public CreditCard(String cardNumber){
+        this.cardNumber = cardNumber;
+    }
+
+    public String getNumber(){
+        return cardNumber;
+    }
+
     public void assignLimit(double limit){
         balance = limit;
         cardLimit = limit;
     }
-    
+
     public double getLimit(){
         return 2000;
     }
-    
+
 
     public void block(){
-        isBlocked = true;  
+        isBlocked = true;
     }
-    
+
     public boolean isBlocked(){
         return isBlocked;
-
+    }
 
     public void withdraw(double amount){
         balance -= amount;
     }
-    
+
     public double getBalance() {
         return balance;
 
     }
+
+
 }
