b22d999d71e0703e6fa59774f6f66ab13496a044
Michał <michal951now@gmail.com>
2018-11-15 12:09:19 +0000

    come changes

--- creditcard-app/src/main/java/pl/jkan/creditcard/App.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/App.java
@@ -1,31 +1,14 @@
 package pl.jkan.creditcard;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class App
 {
     public static void main( String[] args )
     {
-        CreditCardPool pool = new CreditCardPool();
-        CreditCard c1 = new CreditCard("1234");
-        CreditCard c2 = new CreditCard("7657");
-
-        pool.add(c1);
-        pool.add(c2);
-        CreditCardApi = new CreditCardApi(pool);
-
-        Runnable wife = () -> {
-            while(true){
-                api.withdraw("1234",200);
-            }
-        }
-
-        Runnable hausband = () -> {
-            while(true){
-                api.withdraw("1234",50);
-            }
-        }
+        SpringApplication.run(App.class, args);
     }
 }
