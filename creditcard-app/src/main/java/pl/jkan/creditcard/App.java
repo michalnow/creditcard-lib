1b11aeec4a2eac42c636b4173b240811384f489c
Michał <michal951now@gmail.com>
2018-11-15 10:52:25 +0000

    refactor app start

--- creditcard-app/src/main/java/pl/jkan/creditcard/App.java
+++ creditcard-app/src/main/java/pl/jkan/creditcard/App.java
@@ -4,10 +4,28 @@ package pl.jkan.creditcard;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CreditCardPool pool = new CreditCardPool();
+        CreditCard c1 = new CreditCard("1234");
+        CreditCard c2 = new CreditCard("7657");
+
+        pool.add(c1);
+        pool.add(c2);
+        CreditCardApi = new CreditCardApi(pool);
+
+        Runnable wife = () -> {
+            while(true){
+                api.withdraw("1234",200);
+            }
+        }
+
+        Runnable hausband = () -> {
+            while(true){
+                api.withdraw("1234",50);
+            }
+        }
     }
 }
