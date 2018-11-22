7dade6fdb327a1d6c763a9cc238414d5fc85e30c
Michał <michal951now@gmail.com>
2018-11-22 14:26:17 +0000

    moved app to package jkan

---
+++ creditcard-app/src/main/java/pl/jkan/App.java
@@ -0,0 +1,14 @@
+package pl.jkan;
+
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
